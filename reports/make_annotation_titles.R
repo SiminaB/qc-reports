ccdacabd3d25d88a6b58b170373cba1ad9856c0c
Simina M. Boca <smb310@georgetown.edu>
2020-10-16 00:48:39 -0400

    Created make_annotations_titles.R to dump some code there

---
+++ reports/make_annotation_titles.R
@@ -0,0 +1,137 @@
+all_spect_title <- list(
+  text = "All MS/MS Spectra",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1,
+  showarrow = FALSE
+)
+
+id_spect_title <- list(
+  text = "Identified MS/MS Spectra",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1,
+  showarrow = FALSE
+)
+
+all_spect_title_grid <- list(
+  text = "All MS/MS Spectra",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1.15,
+  showarrow = FALSE
+)
+
+id_spect_title_grid <- list(
+  text = "Identified MS/MS Spectra",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1.15,
+  showarrow = FALSE
+)
+
+# Add new layout annotation for MS/MS Distinct Peptides for Fractions by Analytical Sample (Jiahao)
+peptide_title <- list(
+  text = "Distinct Peptides",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1.0,
+  showarrow = FALSE
+)
+
+as_title <- list(
+  text = "Analytical Sample",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "top",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = -0.1,
+  showarrow = FALSE
+)
+
+fr_title <- list(
+  text = "Fraction",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "top",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = -0.1,
+  showarrow = FALSE
+)
+
+# Add new layout annotation for Redundancy
+peptide_rd <- list(
+  text = "Peptide Redundancy",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1.12,
+  showarrow = FALSE
+)
+
+# Add new layout annotation for Identified Protein
+protein_title <- list(
+  text = "Identified Proteins",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1.0,
+  showarrow = FALSE
+)
+
+# Add new layout annotation for PCA
+pca_title <- list(
+  text = "PCA",
+  ##font = f,
+  xref = "paper",
+  yref = "paper",
+  yanchor = "bottom",
+  xanchor = "center",
+  align = "left",
+  x = 0.5,
+  y = 1.0,
+  showarrow = FALSE
+)
+
+# create title for each of the plot (Jiahao)
+title_ALL <- "All MS/MS Spectra" 
+title_ID <-  "Identified MS/MS Spectra"
